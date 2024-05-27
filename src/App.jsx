9553364da714166ff76f7480af9ca2c349e09676
Gboly <gbolahanahmed1@gmail.com>
2024-05-27 10:45:58 +0100

    feat: Setup App.jsx with first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Feyi from "./pages/feyi/feyi";
+import Fullscreen from "./component/fullscreen/Fullscreen";
+import { createContext, useState } from "react";
 
+export const GeneralContext = createContext();
 function App() {
-  const [count, setCount] = useState(0)
+  const [{ src, type }, setFullscreen] = useState({ src: "", type: "" });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <GeneralContext.Provider value={{ setFullscreen }}>
+      <Feyi />
+      {src && <Fullscreen src={src} type={type} />}
+    </GeneralContext.Provider>
+  );
 }
 
-export default App
+export default App;
